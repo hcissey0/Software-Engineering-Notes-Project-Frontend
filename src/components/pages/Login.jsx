aa05b660c87c10cd1141801160d9acb66d9beeb6
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-06-19 13:56:37 +0000

    login page: refined

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,26 +1,77 @@
-import "../../App.css"
-import ThemeToggle from "../atoms/ThemeToggle"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../App.css";
+import ThemeToggle from "../atoms/ThemeToggle";
+import Result from "postcss/lib/result";
 
 const Login = () => {
+  let navigate = useNavigate();
+  const [inputs, setInputs] = useState({});
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs({ ...inputs, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    const form = document.querySelector("form");
+    const data = JSON.stringify(inputs);
+    const response = await fetch("http://192.168.137.168:8000/api/token/", {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const resp_data = await response.json();
+    console.log(resp_data);
+    console.log(response.statusText, response.status);
+
+    if (response.status === 200) {
+      const access = resp_data.access;
+      const refresh = resp_data.refresh;
+      const user_details = {
+        access: access,
+        refresh: refresh,
+      };
+      localStorage.setItem("user_details", JSON.stringify(user_details));
+      form.submit();
+      return true;
+    }
+  };
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <div className="absolute top-5 right-5">
         <ThemeToggle />
       </div>
-      <form className="min-w-96 max-w-sm p-12 rounded-2xl shadow-xl bg-white dark:bg-slate-800">
-        <h1 className="login text-2xl font-bold text-gray-950 mb-4 dark:text-white">Login</h1>
+      <form
+        className="min-w-96 max-w-sm p-12 rounded-2xl shadow-xl bg-white dark:bg-slate-800"
+        method="POST"
+        action="/edit"
+        onSubmit={(e) => {
+          if (handleSubmit(e) === true) {
+            navigate("/");
+          }
+        }}
+      >
+        <h1 className="login text-2xl font-bold text-gray-950 mb-4 dark:text-white">
+          Login
+        </h1>
         <div className="mb-5">
           <label
-            htmlFor="email"
+            htmlFor="username"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Your email
+            Your username
           </label>
           <input
-            type="email"
-            id="email"
+            type="text"
+            id="username"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="username@yourmail.com"
+            placeholder="username"
+            name="username"
+            onChange={handleChange}
             required
           />
         </div>
@@ -35,6 +86,8 @@ const Login = () => {
             type="password"
             id="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            name="password"
+            onChange={handleChange}
             required
           />
         </div>
@@ -58,6 +111,11 @@ const Login = () => {
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={(e) => {
+            if (handleSubmit(e) === true) {
+              navigate("/");
+            }
+          }}
         >
           Submit
         </button>

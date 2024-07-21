383f25ebc9fea0cca12517c916a31dcd66ea7006
isaac-svg <isaacsakyi009@gmail.com>
2024-07-21 01:07:18 +0000

    new data

--- src/components/pages/Edit.jsx
+++ src/components/pages/Edit.jsx
@@ -1,42 +1,44 @@
-import React, {useState } from 'react';
-import ReactQuill from 'react-quill';
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import './edit.css'
+import "./edit.css";
 
-    const modules = {
-      toolbar : [
-        [ {header : [ 1, 2, 3, 4, 5, 6, false ]} ],
-        [ {font : []} ],
-        [ {size : []} ],
-        [ "bold", "italic", "underline", "strike", "blockquote" ],
-        [
-          {list : "ordered"},
-          {list : "bullet"},
-          {indent : "-1"},
-          {indent : "+1"},
-        ],
-        [ "link", "image", "video" ],
-      ],
-    };
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image", "video"],
+  ],
+};
 
 function Edit() {
-  const[value, setValue] = useState("");
+  const [value, setValue] = useState("");
   return (
     <>
-      <div className='container'>
+      <div className="container">
         <div className="row">
           <div className="editor">
-            <ReactQuill theme='snow' value={value} 
-              onChange={()=> setValue(e.target.value)}
-              className='editor-input'
+            <ReactQuill
+              theme="snow"
+              value={value}
+              onChange={() => setValue(e.target.value)}
+              className="editor-input"
               modules={modules}
             />
           </div>
         </div>
-       </div>
+      </div>
     </>
   );
 }
